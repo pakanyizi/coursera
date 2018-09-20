d890198a9112a8c63df79a797c6618c17161e8b4
Don Mutua <print.idd@gmail.com>
2018-09-20 13:17:21 +0300

    Single page application 1

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import { Card, CardImg, CardText,CardBody ,CardTitle, CardSubtitle } from "reactstrap";
+
+const RenderCard = ({item}) => {
+    return (
+        <Card>
+            <CardImg src={item.image} alt={item.name}/>
+            <CardBody>
+                <CardTitle>{item.name}</CardTitle>
+                {item.designation?<CardSubtitle>{item.designation}</CardSubtitle>:null}
+                <CardText>{item.description}</CardText>
+            </CardBody>
+        </Card>
+     );
+}
+
+
 
 const Home = (props) => {
     return (
-        <div className='containers'>
-            <h4>Home</h4>
+        <div className='container'>
+            <div className="row align-items-start">
+                <div className="col-12 col-md m-1">
+                {/* show featured dish */}
+                <RenderCard item={props.dish}/>
+                </div>
+                {/* show featured promotion */}
+                 <div className="col-12 col-md m-1">
+                <RenderCard item={props.promotion}/>
+                </div>
+                {/* show featured leader */}
+                <div className="col-12 col-md m-1">
+                <RenderCard item={props.leader}/>
+                </div>
+            </div>
         </div>
     );
 };
